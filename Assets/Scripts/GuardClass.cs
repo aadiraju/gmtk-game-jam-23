0e92ff3d71bede36fac91280f728839c4549832b
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-07 19:00:55 -0700

    Removing comments

--- Assets/Scripts/GuardClass.cs
+++ Assets/Scripts/GuardClass.cs
@@ -48,32 +48,12 @@ public class GuardClass {
 
     private static void rotateMatrix(int N, int[, ] mat)
     {
-        // Consider all
-        // squares one by one
         for (int x = 0; x < N / 2; x++) {
-            // Consider elements
-            // in group of 4 in
-            // current square
             for (int y = x; y < N - x - 1; y++) {
-                // store current cell
-                // in temp variable
                 int temp = mat[x, y];
- 
-                // move values from
-                // right to top
                 mat[x, y] = mat[y, N - 1 - x];
- 
-                // move values from
-                // bottom to right
-                mat[y, N - 1 - x]
-                    = mat[N - 1 - x, N - 1 - y];
- 
-                // move values from
-                // left to bottom
-                mat[N - 1 - x, N - 1 - y]
-                    = mat[N - 1 - y, x];
- 
-                // assign temp to left
+                mat[y, N - 1 - x] = mat[N - 1 - x, N - 1 - y];
+                mat[N - 1 - x, N - 1 - y] = mat[N - 1 - y, x];
                 mat[N - 1 - y, x] = temp;
             }
         }
