b09ea5e4bc6a284068098e1b7c14e7a52e137736
Brenden T <brenden.trieu@gmail.com>
2023-07-09 00:40:03 -0700

    base back button

--- Assets/Scripts/GoBack.cs
+++ Assets/Scripts/GoBack.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class GoBack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
     }
 }
